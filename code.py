2729049b82a7a349caef8f02645aed94a6300833
Ali Benabdallah <ali.benabdallah008@gmail.com>
2020-10-04 21:34:50 +0200

    Version numpy

--- code.py
+++ code.py
@@ -1,68 +1,81 @@
 import random
+import numpy as np 
 
-map = [] #map 2D
-for i in range(10):
-    map.append([0]*10)
+#Constantes
+N=10
+#Dictionnaire reliant les differents bateaux et leur taille en nombre de case
 dict_bateau = dict() #dictionnaire de bateaux
 dict_bateau = {1:5, 2:4,3:3,4:3,5:2}
 
-def peut_placer(grille,bateau,position,direction):
-    """[summary]
+#Fonction qui génère une grille vide
+def grille_vide():
+    """Genere une grille vide de dimension N*N """
+    return np.zeros(shape=(N,N),dtype = int)
 
+
+def peut_placer(grille,bateau,position,direction):
+    """
+    Teste s'il est possible de placer un bateau dans une direction souhaitee
+    a la case position de la grille
     Args:
         grille (int[][]): map 2D 10x10
         bateau (String): nom du bateau
-        position ((int,int)): position dans la matrice 
+        position (int*int): position dans la matrice 
         direction (int): 1 pour horizontal, 2 pour vertical
 
     Returns:
-        [type]: [description]
+        [type]: [boolean]
     """
     (k,l) = position
-    i=k
-    j=l
+    if (k >= N or l >= N): return False
     if (direction == 1):
-        while (j<10 and j < l + dict_bateau[bateau]):
-            if grille[i][j]!=0:
-                return False
-            j=j+1
+        if (l + dict_bateau[bateau] <= N):
+            return np.array_equal(grille[k,l:l + dict_bateau[bateau]], np.zeros(dict_bateau[bateau],dtype=int))
     else :
-        while (i<10 and i < l + dict_bateau[bateau]):
-            if grille[i][j]!=0:
-                return False
-            i=i+1
-    return True
+        if (k + dict_bateau[bateau] <= N):
+            return np.array_equal(grille[k:k + dict_bateau[bateau],l], np.zeros(dict_bateau[bateau],dtype=int))
+    
 
 def place(grille,bateau,position,direction):
+    """
+    Place un bateau si c'est possible et retourne True sinon retourne False
+    Args:
+        grille (int[][]): map 2D 10x10
+        bateau (String): nom du bateau
+        position (int*int): position dans la matrice 
+        direction (int): 1 pour horizontal, 2 pour vertical
+    """
     (k,l) = position
     i=k
     j=l
     if (peut_placer(grille,bateau,position,direction)):
         if (direction == 1):
-            while (j<10 and j < l + dict_bateau[bateau]):
+            for j in range(l,l+dict_bateau[bateau]):
                 grille[i][j]=bateau
-                j=j+1
         else :
-            while (i<10 and i < l + dict_bateau[bateau]):
+            for i in range(k,k+dict_bateau[bateau]):
                 grille[i][j]=bateau
-                i=i+1
         return True
     return False
 
 def place_alea(grille,bateau):
+    """
+        Place un bateau de position et direction aleatoire dans la grille
+    Args:
+        grille (int[][]): map 2D 10x10
+        bateau (String): nom du bateau
+    """
     b=False
     while not b:
         position = (random.randint(0,9),random.randint(0,9))
         direction = random.randint(1,2)
-        print("position : "+ position)
-        print("direction : "+ direction)
         b = place(grille,bateau,position,direction)
-    return
+    return b
 
-place(map,1,(0,0),1)
+#test
+map = grille_vide()
+place(map,1,(0,5),1)
 place_alea(map,3)
 place_alea(map,3)
 place_alea(map,3)
-for i in range(10):
-    print(map[i])
-print ("testok")
+print(map)
