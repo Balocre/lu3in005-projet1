497d27a14c692e1358ae9b944c025a089dc96254
Ali Benabdallah <ali.benabdallah008@gmail.com>
2020-10-26 19:45:55 +0100

    Exo 2.6 fait

---
+++ Senseur.py
@@ -0,0 +1,100 @@
+import random
+import numpy as np
+
+
+class Senseur():
+    def __init__(self, n, p,repar):
+        """
+        Constructeur d'une classe senseur, initialise les dimensions et la repartition :
+        Args :
+            n (int) : nombre de lignes
+            p (int) : nombre de colonnes
+            repar (int) : repartition - 1 pour centree - 2 en bordure - 3 gauche nul - sinon aleatoire
+        """
+        mat_p = self.genere_mat_proba_senseur(n,p,repar) #matrice de probabilite
+        self.place_senseur(n,p) #initialise l'attribut pos
+        print(self.pos) 
+
+    def place_senseur(self,n,p):
+        """
+        Initialise l'attribut pos qui correspond au tuple (i,j) représentant la position
+        du sous-marin
+        Args:
+        n (int) : nombre de lignes
+        p (int) : nombre de colonnes
+        """
+        s=0
+        k = random.random()
+        for i in range(n):
+            for j in range(p):
+                if (k < s + self.mat_p[i][j]):
+                    self.pos=(i,j)
+                    return None
+                s = s + self.mat_p[i][j]
+        print("Erreur place_senseur")
+        return None
+
+    def genere_mat_proba_senseur(self,n,p,repar):
+        """Retourne une matrice de probabilité N*P selon une distribution repar.
+        Args : 
+            n (int): nombre de lignes
+            p (int): nombre de colonnes
+            repar (int): repartition choisie. 1 pour centree, 2 pour favoriser la bordure
+                    3 pour defavoriser le flanc gauche, sinon pour aleatoire
+        """
+        self.mat_p = np.empty((n,p), dtype=float)
+        c = (n+p)*50
+        #repartition aleatoire, repar = 0
+        for i in range(n):
+            for j in range(p):
+                self.mat_p[i][j]=float(random.randint(0,c))
+        if (repar==1):
+        #repartition centree
+            for i in range(int(n/2),int(n/2+1)):
+                for j in range(int(p/2),int(p/2+1)):
+                    self.mat_p[i][j]=float(random.randint(4*c,6*c))
+        
+        elif (repar == 2):
+            #repartition bords
+            for i in [0,n-1]:
+                for j in range(p):
+                    self.mat_p[i][j]=float(random.randint(3*c,4*c))
+            for j in [0,n-1]:
+                for i in range(n):
+                    self.mat_p[i][j]=float(random.randint(3*c,4*c))
+        elif (repar == 3):
+        #eviter le flanc gauche
+            for i in range(n):
+                for j in range(2):
+                    if (j<p):
+                        self.mat_p[i][j]=0
+        d = np.sum(self.mat_p,dtype=float)
+        for i in range (n):
+            for j in range (p):
+                self.mat_p[i][j]= self.mat_p[i][j]/d
+        print(self.mat_p)
+        print(np.sum(self.mat_p,dtype=float))
+        return None
+
+    def cherche(self,ps):
+        """
+        Cherche le sous-marin
+        Args:
+            ps (float) : Correspond à la probabilité que le senseur trouve le sous marin
+            si celui-ci se trouve en position self.pos
+        """
+        cpt=0
+        
+        while(True):
+            cpt+=1
+            (i,j) = np.unravel_index(np.argmax(self.mat_p), self.mat_p.shape)
+            if (i,j)==self.pos:
+                k = random.random()
+                if k < ps:
+                    print("Senseur trouvé en pos "+ str((i,j)))
+                    return cpt
+            self.mat_p = self.mat_p / (1 - self.mat_p[i][j]*ps)
+            self.mat_p[i][j] *= 1-ps
+
+s = Senseur(5,5,0)
+print(s.cherche(0.1))
---
+++ tests.py
@@ -0,0 +1,209 @@
+from code import *
+import unittest
+import numpy as np
+
+class TestPartie1(unittest.TestCase):
+
+    def setUp(self):
+        self.bateaux = {1:5, 2:4, 3:3, 4:3, 5:2}
+
+        self.grille_vierge = np.zeros( (10,10) )
+
+        self.grille = np.zeros( (10, 10) )
+        self.grille[1, 2:5] = 4
+        self.grille[3, 3:7] = 2
+        self.grille[5:8, 4] = 3
+        self.grille[7:9, 1] = 5
+        self.grille[5:10, 9] = 1
+        #[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 4, 4, 4, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 2, 2, 2, 2, 0, 0, 0],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 0, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 5, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 5, 0, 0, 0, 0, 0, 0, 0, 1],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
+
+    def test_peut_placer(self):
+        self.assertTrue(peut_placer(self.bateaux, self.grille_vierge, 1, (2, 2), "+x"))
+        self.assertFalse(peut_placer(self.bateaux, self.grille_vierge, 1, (10, 10), "+y"))
+        self.assertFalse(peut_placer(self.bateaux, self.grille, 1, (5, 9), "+x"))
+
+    def test_place(self):
+        self.assertEqual(place(self.bateaux, self.grille_vierge, 1, (0, 0), "+x"), ((0, 0), (1, 0), (2, 0), (3, 0), (4, 0)))
+        self.assertIsNone(place(self.bateaux, self.grille, 1, (0, 3), "+x"))
+
+    def test_place_alea(self):
+        self.assertFalse(place_alea(self.bateaux, self.grille_vierge, 1))
+
+    def test_affiche(self):
+        affiche(self.grille)
+    
+    def test_eq(self):
+        self.assertFalse(eq(self.grille, self.grille_vierge))
+        self.assertTrue(eq(self.grille, self.grille ))
+
+        with self.assertWarns(Warning):
+           eq(self.grille, np.zeros( (1, 1) ))
+
+    def test_genere_grille(self):
+        genere_grille(self.bateaux)
+
+class TestBataille(unittest.TestCase):
+
+    def setUp(self):
+        self.bateaux = {1:5, 2:4, 3:3, 4:3, 5:2}
+
+        self.grille = np.zeros( (10,10) )
+
+        self.grille[1, 2:5] = 4
+        self.grille[3, 3:7] = 2
+        self.grille[5:8, 4] = 3
+        self.grille[7:9, 1] = 5
+        self.grille[5:10, 9] = 1
+        #[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 4, 4, 4, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 2, 2, 2, 2, 0, 0, 0],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 0, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 5, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 5, 0, 0, 0, 0, 0, 0, 0, 1],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
+
+        self.bataille_random = Bataille(self.bateaux)
+
+        self.bataille_init = Bataille(self.bateaux, self.grille)
+
+    def test_joue(self):
+        self.assertNotEqual(self.bataille_init.grille[1][2], self.bataille_init.grille_decouverte[1][2])
+        self.assertEqual(self.bataille_init.joue( (1, 2) ), 4)
+        self.assertEqual(self.bataille_init.grille[1][2], self.bataille_init.grille_decouverte[1][2])
+
+        self.assertIsNone(self.bataille_init.joue( (0, 0) ))
+
+        with self.assertRaises(ValueError):
+            self.bataille_init.joue( (-1, -1) )
+    
+    def test_victoire(self):
+        self.assertFalse(self.bataille_init.victoire())
+        for i in range(0, 10):
+            for j in range(0, 10):
+                self.bataille_init.joue( (i, j) )
+        self.assertTrue(self.bataille_init.victoire())
+
+class TestJourAlea(unittest.TestCase):
+    
+    def setUp(self):
+        self.bateaux = {1:5, 2:4, 3:3, 4:3, 5:2}
+
+        self.grille = np.zeros( (10, 10) )
+        self.grille[1, 2:5] = 4
+        self.grille[3, 3:7] = 2
+        self.grille[5:8, 4] = 3
+        self.grille[7:9, 1] = 5
+        self.grille[5:10, 9] = 1
+        #[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 4, 4, 4, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 2, 2, 2, 2, 0, 0, 0],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 0, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 5, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 5, 0, 0, 0, 0, 0, 0, 0, 1],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
+
+        self.joueur_alea = JoueurAlea(Bataille(self.bateaux, self.grille))
+
+    def test_joue(self):
+        pos_jouee = self.joueur_alea.joue()
+        self.assertIsInstance(pos_jouee, tuple)
+        print("Le joueur à joué en position : {}".format(pos_jouee))
+
+    def test_epuisement(self):
+        print("Liste des coups joués par le joueur aléaoire")
+        while c := self.joueur_alea.joue():
+            print("{}".format(c))
+
+        self.assertTrue(self.joueur_alea.bataille.victoire())
+
+class TestJoueurHeur(unittest.TestCase):
+
+    def setUp(self):
+        self.bateaux = {1:5, 2:4, 3:3, 4:3, 5:2}
+
+        self.grille = np.zeros( (10, 10) )
+        self.grille[1, 2:5] = 4
+        self.grille[3, 3:7] = 2
+        self.grille[5:8, 4] = 3
+        self.grille[7:9, 1] = 5
+        self.grille[5:10, 9] = 1
+        #[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 4, 4, 4, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 2, 2, 2, 2, 0, 0, 0],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 0, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 5, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 5, 0, 0, 0, 0, 0, 0, 0, 1],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
+
+        self.joueur_heur = JoueurHeur(Bataille(self.bateaux, self.grille))
+
+    def test_joue(self):
+        pos_jouee = self.joueur_heur.joue()
+        self.assertIsInstance(pos_jouee, tuple)
+        print("Le joueur à joué en position : {}".format(pos_jouee))
+
+    def test_epuisement(self):
+        print("Liste des coups joués par le joueur aléaoire")
+        while c := self.joueur_heur.joue():
+            # affiche(self.joueur_heur.bataille.grille_decouverte)
+            print("{}".format(c))
+
+        self.assertTrue(self.joueur_heur.bataille.victoire())
+
+class TestJoueurProba(unittest.TestCase):
+
+    def setUp(self):
+        self.bateaux = {1:5, 2:4, 3:3, 4:3, 5:2}
+
+        self.grille = np.zeros( (10, 10) )
+        self.grille[1, 2:5] = 4
+        self.grille[3, 3:7] = 2
+        self.grille[5:8, 4] = 3
+        self.grille[7:9, 1] = 5
+        self.grille[5:10, 9] = 1
+        #[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 4, 4, 4, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 2, 2, 2, 2, 0, 0, 0],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # [0, 0, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 0, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 5, 0, 0, 3, 0, 0, 0, 0, 1],
+        # [0, 5, 0, 0, 0, 0, 0, 0, 0, 1],
+        # [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
+
+        self.joueur_proba = JoueurHeur(Bataille(self.bateaux, self.grille))
+
+    def test_joue(self):
+        pos_jouee = self.joueur_proba.joue()
+        self.assertIsInstance(pos_jouee, tuple)
+        print("Le joueur à joué en position : {}".format(pos_jouee))
+
+    def test_epuisement(self):
+        print("Liste des coups joués par le joueur aléaoire")
+        while c := self.joueur_proba.joue():
+            affiche(self.joueur_proba.bataille.grille_decouverte)
+            print("{}".format(c))
+
+        self.assertTrue(self.joueur_proba.bataille.victoire())
+
+if __name__ == '__main__':
+    unittest.main()
